dc902b202b61aa01b0b19dde7b657542ba00d342
Vinicius Faria <artixsproductions@gmail.com>
2025-02-24 21:23:13 -0300

    feat: continued working on genetic algorithm, version without elitism didn't work well, so will try with elitism

--- src/GA.jl
+++ src/GA.jl
@@ -1,8 +1,11 @@
 module GA
 
 import Random
+include("TimeFunctions.jl")
+include("AcceleratedDubins.jl")
+include("Helper.jl")
 
-struct GAParams{T1,T2,T3}
+struct GAParams{T1,T2,T3,T4}
     popsize::Int
     cxpb::Float64
     mutpb::Float64
@@ -10,7 +13,10 @@ struct GAParams{T1,T2,T3}
 
     selection::T1
     crossover::T2
-    mutation::T3
+    mutation!::T3
+
+    op_params::T4
+    perturb_chance::Float64 #TODO add chance to perturb waypoints -> random waypoint or fastest speed or highest score
 end
 
 mutable struct Individual{T}
@@ -21,25 +27,35 @@ end
 
 function evolution(params::GAParams, fitness_fun, initial_genomes)
     #Evaluate initial individuals
-    population = map(genome -> Individual(genome, fitness_fun(genome), true), initial_genomes)
+    population = map(genome -> Individual(genome, fitness_fun(params.op_params, genome), true), initial_genomes)
+    sort!(population, by=x->x.fitness, rev=true)
 
-    for gen in 1:ngen #Run generations
+    #TODO insert elitism, maybe also dont select from offspring and population?
+    for gen in 1:params.ngen #Run generations
         #Generate our offspring
-        offspring = varAnd(population, params, cxpb, mutpb)
+        offspring = params.selection(population, params.popsize)
+        #Vary individuals
+        offspring = varAnd(population, params, params.cxpb, params.mutpb)
 
         #Re-evaluate invalid fitnesses
         for i in eachindex(offspring)
             if !offspring[i].valid
-                offspring[i].fitness = fitness_fun(offspring[i].genome)
+                offspring[i].fitness = fitness_fun(params.op_params, offspring[i].genome)
                 offspring[i].valid = true
             end
         end
 
-        #Select next generation, select from population AND offspring!
-        population = params.select(vcat(population, offspring), params.popsize)
+        #Reconstruct the population from elite individuals + offspring
+        #TODO
+
     end
 
     return population
+end
+
+#Calc fitness
+function ind_fitness(op_params, seq)
+    return Helper.calculate_seq_results(op_params,seq)[1]
 end
 
 #Apply both crossover and mutation on individuals
@@ -48,14 +64,15 @@ function varAnd(population, params::GAParams, cxpb, mutpb)
     l = length(offspring)
     for i in 2:2:l
         if rand() < cxpb
-            offspring[i-1].genome, offspring[i].genome = params.crossover(offspring[i-1].genome, offspring[i].genome)
+            #TODO this can be optimized a little bit, since we are copying again in the function
+            offspring[i-1].genome, offspring[i].genome = params.crossover(offspring[i-1].genome, offspring[i].genome, params)
             offspring[i-1].valid = offspring[i].valid = false
         end
     end
 
     for i in eachindex(offspring)
         if rand() < mutpb
-            offspring[i].genome = params.mutation(offspring[i].genome)
+            params.mutation!(offspring[i].genome, params)
             offspring[i].valid = false
         end
     end
@@ -63,8 +80,21 @@ function varAnd(population, params::GAParams, cxpb, mutpb)
     return offspring
 end
 
+#Tournament selection
+function selection_tournament(inds, k, tournsize=2)
+    chosen = []
+    for _ in 1:k
+        # Choose tournsize number aspirants
+        aspirants = [rand(inds) for _ in 1:tournsize]
+        # Choose the one with the max value
+        push!(chosen, argmax(x->x.fitness, aspirants))
+    end
+
+    return chosen
+end
+
 #Order crossover, adapted from Evolutionary.jl
-function order_crossover(v1::T, v2::T; rng=Random.default_rng()) where {T <: AbstractVector}
+function order_crossover(v1::T, v2::T, params, rng=Random.default_rng()) where {T <: AbstractVector}
     s = length(v1)
     from, to = rand(rng, 1:s, 2) #You actually can switch index 1 here
     from, to = from > to ? (to, from)  : (from, to)
@@ -77,18 +107,19 @@ function order_crossover(v1::T, v2::T; rng=Random.default_rng()) where {T <: Abs
     in_c1, in_c2 = Set([x[1] for x in c1]), Set([x[1] for x in c2])
     
     # Fill in from parents -> Get first element
-    k = from == 1 ? to+1 : 1 #child1 index before modifications
-    j = from == 1 ? to+1 : 1 #child2 index before modifications
+    k = 1
+    j = 1 
+    #We also dont need to check if to+1 is in bounds, because if from == 1 and to+1 > len(v1), for loop below doesnt run
 
     for i in vcat(1:from-1, to+1:s)
         while in(v1[k][1],in_c1) #while value is already in new child
-            k = k+1 > s ? 1 : k+1
+            k = k+1 # No need to wrap around here
         end
         c1[i] = v1[k] #Set value
         push!(in_c1, v1[k][1])
 
         while in(v2[j][1],in_c2) #Do the same for c2
-            j = j+1 > s ? 1 : j+1
+            j = j+1
         end
         c2[i] = v2[j]
         push!(in_c2, v2[j][1])
@@ -96,15 +127,14 @@ function order_crossover(v1::T, v2::T; rng=Random.default_rng()) where {T <: Abs
     return c1, c2
 end
 
-function mutation(recombinant::T; rng=Random.default_rng()) where {T <: AbstractVector}
+function mutation(recombinant::T, params, rng=Random.default_rng()) where {T <: AbstractVector}
     method = rand(rng, [insertion, swap2])
 
-    println(method)
-    return method(recombinant)
+    return method(recombinant, params)
 end
 
 #Takes a random point and inserts elsewhere
-function insertion(recombinant::T; rng=Random.default_rng()) where {T <: AbstractVector}
+function insertion(recombinant::T, params, rng=Random.default_rng()) where {T <: AbstractVector}
     l = length(recombinant)
     from, to = rand(rng, 2:l, 2) #Position 1 cant be swapped
     val = recombinant[from]
@@ -113,18 +143,23 @@ function insertion(recombinant::T; rng=Random.default_rng()) where {T <: Abstrac
 end
 
 #Swaps 2 points
-function swap2(recombinant::T; rng=Random.default_rng()) where {T <: AbstractVector}
+function swap2(recombinant::T, params, rng=Random.default_rng()) where {T <: AbstractVector}
     l = length(recombinant)
     p1,p2 = rand(rng, 2:l, 2) #Position 1 cant be swapped
-    evol.swap!(recombinant, p1, p2)
+    recombinant[p1], recombinant[p2] = recombinant[p2], recombinant[p1]
     return recombinant
 end
 
 #Changes waypoints
-function waypoint_perturb(recombinant::T, rng=Random.default_rng()) where {T <: AbstractVector}
+function waypoint_perturb(recombinant::T, params, rng=Random.default_rng()) where {T <: AbstractVector}
     l = length(recombinant)
-    idx = rand(rng, 1:l)
-    recombinant[idx] = (recombinant[idx][1], 1, 1) #TODO find a way to pass this as param
+
+    h,s = params.op_params.graph.num_headings, params.op_params.graph.num_speeds
+    for i in eachindex(recombinant)
+        if rand() < params.perturb_chance
+            recombinant[idx] = (recombinant[idx][1], rand(1:s), rand(1:h))
+        end
+    end
 end
 
 end
